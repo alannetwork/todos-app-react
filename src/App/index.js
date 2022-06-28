9ebd4eba9e62da7343981f3cdd1822542f3f195d
cloudmex-alan <alanestrada@cloudmex.io>
2022-06-28 10:45:28 -0600

    Added react effects to app

--- src/App/index.js
+++ src/App/index.js
@@ -1,3 +1,4 @@
+import { react } from "@babel/types";
 import React from "react";
 import { AppUI } from "./AppUI";
 
@@ -8,20 +9,61 @@ import { AppUI } from "./AppUI";
   {text:'Estudiar en platzi',completed: false },
 ]; */
 
-function App(props) {
+function useLocalStorage(itemName,initialValue){
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
 
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  React.useEffect(()=>{
+    setTimeout(()=>{
+      try {
+      
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+      
+        if (!localStorageItem){
+          localStorage.setItem(itemName,JSON.stringify(initialValue));
+          parsedItem=initialValue;
+        }else{
+          parsedItem = JSON.parse(localStorageItem);
+        }
+        setItem(parsedItem);
+        setLoading(false);
+    
+      } catch (error) {
+        setError(error);        
+      }
+    },1000);
+  });
 
-  let parsedTodos;
 
-  if (!localStorageTodos){
-    localStorage.setItem('TODOS_v1',JSON.stringify([]));
-    parsedTodos=[];
-  }else{
-    parsedTodos = JSON.parse(localStorageTodos);
+  const saveItem = (newItem) =>{
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName,stringifiedItem);
+      setItem(newItem);
+      
+    } catch (error) {
+      setError(error);
+    }
   }
 
-  const [todos, setTodos] = React.useState(parsedTodos);
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+
+}
+
+function App(props) {
+  const {
+    item:todos,
+    saveItem:saveTodos,
+    loading,
+    error,
+  } = useLocalStorage('TODOS_V1',[]);
 
   const [searchValue,setSearchValue] = React.useState('');
   const totalTodos = todos.length;
@@ -42,12 +84,6 @@ function App(props) {
 
   }
 
-  const saveTodos = (newTodos) =>{
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1',stringifiedTodos);
-    setTodos(newTodos);
-  }
-
   const completeTodos = (text) =>{
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
@@ -62,9 +98,10 @@ function App(props) {
     return saveTodos(newTodos);
   };
 
-
   return (
     <AppUI
+      loading={loading}
+      error={error}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
